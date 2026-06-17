bb331d95c92a574d4d4ce96c38d6b073a1a8de0a
kumarsajan87396-del <kumarsajan87396@gmail.com>
2026-06-17 14:04:18 +0530

    jarvis_service.py

---
+++ jarvis_service.py
@@ -0,0 +1,10 @@
+import time
+
+def offline_voice_listener():
+    print("Jarvis Background Engine Initialized.")
+    while True:
+        time.sleep(2) 
+
+if __name__ == '__main__':
+    offline_voice_listener()
+  
